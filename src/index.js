9487f41227ac5f4039a2611c61b4b61eba220a89
Maycon <mayconfelipe164@gmail.com>
2022-05-04 13:17:07 -0300

    build: adding cors

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+const cors = require('cors');
 const express = require('express');
 const pratoRoute = require('./routes/pratoRoute');
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
