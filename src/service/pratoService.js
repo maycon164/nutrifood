5bf71c01b68283787060a60744baaf2f1e596c72
maycon <mayconfelipe164@gmail.com>
2022-05-03 22:21:06 -0300

    feat: service prato

--- src/service/pratoService.js
+++ src/service/pratoService.js
@@ -1,3 +1,4 @@
+const { update } = require('../model/Prato');
 const Prato = require('../model/Prato');
 
 async function getPratos(){
@@ -5,6 +6,33 @@ async function getPratos(){
   return pratos;
 }
 
+async function savePrato(prato){
+  const savedPrato = await Prato.create(prato);
+  return savedPrato;
+}
+
+async function updatePrato(idPrato, prato){
+  const {nome, tipo, preco} = prato;
+  const updatedPrato = await Prato.findOne({where: {id: idPrato}});
+  
+  updatedPrato.nome = nome;
+  updatedPrato.tipo = tipo;
+  updatedPrato.preco = preco;
+
+  await updatedPrato.save();
+
+  return updatedPrato;
+}
+
+async function deletePrato(idPrato){
+  const prato = await Prato.destroy({where: {id: idPrato}});
+  return prato == 1? true: false;
+}
+
+
 module.exports = {
-  getPratos
+  getPratos,
+  savePrato,
+  updatePrato,
+  deletePrato
 };
