fd3bf20a617512a1089e8f47e9299dd9a52e02b4
Maycon <mayconfelipe164@gmail.com>
2022-05-15 22:27:49 -0300

    feat: user model and order model

--- nutrifood/src/connection/database.provider.ts
+++ nutrifood/src/connection/database.provider.ts
@@ -1,5 +1,7 @@
 import { Sequelize } from 'sequelize-typescript';
+import { Order } from 'src/orders/entities/order.entitie';
 import { Snack } from 'src/snack/entities/snack.entitie';
+import { User } from 'src/users/entities/user.entitie';
 
 export const databaseProviders = [
   {
@@ -17,7 +19,13 @@ export const databaseProviders = [
           underscored: true,
         },
       });
-      sequelize.addModels([Snack]);
+      sequelize.addModels([Snack, User, Order]);
+
+      User.hasMany(Order, { foreignKey: 'id_user' });
+      Order.belongsTo(User, { foreignKey: 'id_user' });
+      Snack.hasMany(Order, { foreignKey: 'id_snack' });
+      Order.belongsTo(Snack, { foreignKey: 'id_snack' });
+
       await sequelize.sync();
       return sequelize;
     },
