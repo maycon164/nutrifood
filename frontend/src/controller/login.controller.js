bfc970ebe0645beb704e3bf2891ec50048ab6d23
Maycon Huanca <maycon.huanca@e-core.com>
2022-06-05 17:07:36 -0300

    front: make login and save token in cache

---
+++ frontend/src/controller/login.controller.js
@@ -0,0 +1,26 @@
+const emailEl = document.getElementById("email");
+const passwordEl = document.getElementById("password");
+const btnMakeLoginEl = document.getElementById("btn-make-login");
+
+btnMakeLoginEl.addEventListener("click", async () => {
+
+    const email = emailEl.value;
+    const password = passwordEl.value;
+
+    if (email == '' || password == '') {
+        alert('email ou senha estao vazios');
+    } else {
+
+        const accessToken = await makeLogin(
+            {
+                username: email,
+                password: password
+            });
+
+        if (accessToken) {
+            setTokenAccess(accessToken)
+        }
+
+    }
+
+})
--- frontend/src/controller/requests.js
+++ frontend/src/controller/requests.js
@@ -1,42 +1,66 @@
 
-async function getAllSnacks(category=""){
+async function getAllSnacks(category = "") {
     return await fetch(`http://localhost:3000/snack/${category}`).then(response => response.json());
 }
 
-async function getAllOrders(){
+async function getAllOrders() {
     return await fetch('http://localhost:3000/orders').then(response => response.json());
 }
 
-async function getSnack(id){
+async function getSnack(id) {
     return await fetch(`http://localhost:3000/snack/snack/${id}`).then(response => response.json());
 }
 
-async function makeAOrder(orderObject){
+async function makeLogin(loginObject) {
+    return await fetch(`http://localhost:3000/auth/login`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(loginObject)
+    })
+        .then(response => response.json())
+        .then(obj => obj.access_token)
+}
+
+
+async function makeAOrder(orderObject) {
     return await fetch(`http://localhost:3000/orders`, {
         method: `POST`,
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
-          },
-        body: JSON.stringify(orderObject) 
+        },
+        body: JSON.stringify(orderObject)
     })
 }
 
-async function deleteSnack(id){
+async function deleteSnack(id) {
     return await fetch(`http://localhost:3000/snack/${id}`, {
         method: 'DELETE'
     })
-    .then(response => response.json())
-    .then(json => json.message)
+        .then(response => response.json())
+        .then(json => json.message)
 }
 
-async function updateSnack(id){
-    alert(`TODO UPDATE ${id}`)    
+async function updateSnack(id) {
+    alert(`TODO UPDATE ${id}`)
 }
 
-async function insertNewSnackRequest(snack){
+async function insertNewSnackRequest(snack) {
     return await fetch("http://localhost:3000/snack", {
         method: "POST",
         body: snack
     }).then(response => response.json());
 }
+
+function setTokenAccess(token) {
+    localStorage.setItem("token", token);
+}
+
+function getTokenAccess() {
+    const token = localStorage.getItem("token");
+    console.log(token);
+    return token;
+} 
